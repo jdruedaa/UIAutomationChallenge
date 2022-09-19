292655b1038bd8498172de0b4f453a31a1c3a9db
jdruedaa <jd.ruedaa@uniandes.edu.co>
2022-09-18 19:31:55 -0500

    Added Failed LogIn Attempt Test

--- UIAutomation/src/test/java/org/tmdb/pages/LogInPage.java
+++ UIAutomation/src/test/java/org/tmdb/pages/LogInPage.java
@@ -1,12 +1,16 @@
 package org.tmdb.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LogInPage extends BasePage{
     @FindBy(css = "#login_form #username")
@@ -16,7 +20,7 @@ public class LogInPage extends BasePage{
     @FindBy(css = "#login_form #login_button")
     private WebElement submitLogInFormButton;
 
-    @FindBy(css = ".error_status.card .open.no_click")
+    @FindBy(css = ".error_status.card")
     private WebElement logInErrorStatusCard;
 
     public LogInPage(WebDriver driver) {
@@ -29,5 +33,21 @@ public class LogInPage extends BasePage{
         usernameTextInputField.sendKeys(username);
         passwordTextInputField.sendKeys(password);
         explicitWait.until(ExpectedConditions.elementToBeClickable(submitLogInFormButton)).click();
+    }
+
+    public List<String> getLogInErrorMessages()
+    {
+        WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        List<String> messages = new ArrayList<>();
+        explicitWait.until(ExpectedConditions.visibilityOf(logInErrorStatusCard));
+        WebElement mainErrorMessage = logInErrorStatusCard.findElement(By.cssSelector(".background_color"));
+        System.out.println(Color.fromString(mainErrorMessage.getCssValue("background-color")).toString());
+        messages.add(mainErrorMessage.getText());
+        List<WebElement> providedErrorMessages = logInErrorStatusCard.findElements(By.cssSelector(".content li"));
+        for(WebElement webElement:providedErrorMessages)
+        {
+            messages.add(webElement.getText());
+        }
+        return messages;
     }
 }
--- UIAutomation/src/test/java/org/tmdb/tests/login/LogInTests.java
+++ UIAutomation/src/test/java/org/tmdb/tests/login/LogInTests.java
@@ -1,21 +1,20 @@
 package org.tmdb.tests.login;
 
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.tmdb.pages.HomePage;
 import org.tmdb.pages.LogInPage;
 import org.tmdb.pages.ProfilePage;
 import org.tmdb.utils.Hooks;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class LogInTests extends Hooks {
 
-    //Mirar Listeners de testng para cosas como screenshots. Y dataproviders, especialmente para data-driven frameworks
-
+    private final int numberOfErrorMessages = 3;
     @Test
-    //@Parameters({"correctUsername", "correctPassword"})
-    //public void SuccessfulLogInTest(String username, String password)
     public void SuccessfulLogInTest()
     {
         HomePage home = new HomePage(driver);
@@ -35,22 +34,18 @@ public class LogInTests extends Hooks {
         }
     }
 
-    /*
     @Test
-    @Parameters({"correctUsername", "wrongPassword"})
-    public void FailedLogInWithWrongCredentialsTest(String correctUsername, String wrongPassword)
+    @Parameters("wrongPassword")
+    public void FailedLogInWithWrongCredentialsTest(String wrongPassword)
     {
+        String username = System.getenv("MovieDB_username");
         HomePage home = new HomePage(driver);
         LogInPage logInPage = home.logIn();
-        logInPage.logInToUserAccount(correctUsername, wrongPassword);
-        try
-        {
-
-        }
-        catch (NoSuchElementException noSuchElementException)
-        {
-            System.out.println("Failed Login with Wrong Credentials Test Failed");
-        }
+        logInPage.logInToUserAccount(username, wrongPassword);
+        List<String> messages = logInPage.getLogInErrorMessages();
+        int foundNumberOfErrorMessages = messages.size();
+        Assert.assertEquals(foundNumberOfErrorMessages, numberOfErrorMessages, "There is a different number of " +
+                "error messages (" + foundNumberOfErrorMessages + ") compared to the expected (" + numberOfErrorMessages
+                + ")");
     }
-    */
 }
